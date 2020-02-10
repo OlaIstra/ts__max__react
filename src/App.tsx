eae5307d6e14ed8ada8a67d19bfbf21360d8291f
OlaIstra <38282991+OlaIstra@users.noreply.github.com>
2020-02-10 18:48:44 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { ITodo } from "./todo.models";
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      { id: Date.now().toString(), text: text }
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => {
+        return todo.id !== todoId;
+      });
+    });
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
+
+export default App;
